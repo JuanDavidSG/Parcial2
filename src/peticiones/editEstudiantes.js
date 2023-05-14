59e6642d39cd6b2e30e5e0cf5c37148028c5be51
juan david sanchez <Juansangon@unisabana.edu.co>
2023-05-14 17:56:19 -0500

    Se crea la peticion 'editEstudiantes' para permitir conectar con el backend y asi poder editar y actualizar el estudiante

--- src/peticiones/editEstudiantes.js
+++ src/peticiones/editEstudiantes.js
@@ -1,12 +1,12 @@
-export const editEstudiantes = async (estudiante) =>{
+export const editEstudiantes = async (estudiante) => {
     const url = `http://localhost:8086/estudiante/editar/${estudiante.id}`
-    const resp = await fetch(url,{
-        method:'PUT',
-        headers:{
+    const resp = await fetch(url, {
+        method: 'PUT',
+        headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(estudiante)
     })
-    const data= await resp.json();
+    const data = await resp.json();
 
 }
